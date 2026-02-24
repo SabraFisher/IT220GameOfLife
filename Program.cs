ef5b4df7700143bc4e31fc2bda200609be53861d
Sabra Fisher <sabra.leigh.fisher@gmail.com>
2026-02-23 21:43:34 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Create a Board class that can have a 2d array of cells, where each cell can be alive or dead. 
+// Implement methods to display the board in the console and clear a board state.
+
+namespace IT220GameOfLife
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
